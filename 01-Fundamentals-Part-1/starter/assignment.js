5e9354fbb854796fad7a1071406cc0cc374d1ebd
Dhruv Chauhan <79371981+Dhruv-o16@users.noreply.github.com>
2026-08-04 18:56:52 +0530

    Completed practice for Coding exercise 3: challenge #3 BONUS 1

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -131,7 +131,27 @@ if (scoreDolphins > scoreKoalas) {
   console.log("Dolpins win the trophy");
 } else if (scoreKoalas > scoreDolphins) {
   console.log("Koalas win the trophy");
-} else {
+} else if (scoreDolphins === scoreKoalas) {
   console.log("Both win the trophy");
 }
 */
+
+// BONUS 1
+const scoreDolphins = (97 + 112 + 80) / 3;
+const scoreKoalas = (109 + 95 + 50) / 3;
+
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log("Dolpins win the trophy");
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+  console.log("Koalas win the trophy");
+} else if (
+  scoreDolphins === scoreKoalas &&
+  scoreDolphins >= 100 &&
+  scoreKoalas >= 100
+) {
+  console.log("Both win the trophy");
+} else {
+  console.log("No one wins the trophy 😭");
+}
