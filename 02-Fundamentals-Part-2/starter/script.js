bf78705f1f6652b133e871a98bf3cdb22342a411
Dhruv Chauhan <79371981+Dhruv-o16@users.noreply.github.com>
2026-08-19 16:19:31 +0530

    Learn difference between functions declaration and function expression

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -32,3 +32,29 @@ console.log(appleJuice);
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 */
+
+// Function declaration
+function calcAge1(birthYear) {
+  // function declaration can be defined later as below. You can call a function declaration before you define it but it not such a good idea in many cases but you can do it if necessary.
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+
+// This is an example of function expression which we cannot define later like function declaration.
+// Function expression cannot be define later like as below. And internally this happens because of a process called hoisting.
+const age3 = calcAge3(1991); // Like you cannot define it like this in expression.
+
+const calcAge3 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+console.log(age3);
