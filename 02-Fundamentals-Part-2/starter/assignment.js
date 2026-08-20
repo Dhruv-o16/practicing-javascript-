d10f478484d57669d85390548ec036e9914b9ef8
Dhruv Chauhan <79371981+Dhruv-o16@users.noreply.github.com>
2026-08-20 17:48:45 +0530

    Practicing functions

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -22,3 +22,15 @@ console.log(age1, age2);
 const calcAge3 = birthYear => 2037 - birthYear;
 const age3 = calcAge3(1991);
 console.log(age3);
+
+// Multiple-line arrow function
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;     This one is first method.
+  // second is below.
+  return `${firstName} retires in ${retirement} years.`;
+};
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
