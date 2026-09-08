7f284ceddffdbf4c822016c072cac81066975d83
Dhruv Chauhan <79371981+Dhruv-o16@users.noreply.github.com>
2026-09-08 16:10:29 +0530

    Coding exercise: Functions test 2

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -76,24 +76,32 @@ console.log(yearsUntilRetirement(1970, 'Mike'));
 */
 
 // Function Coding Exercise: Challenge #1
-
+/*
 const calcAverage = (a, b, c) => (a + b + c) / 3;
 console.log(calcAverage(3, 4, 5));
 
 // Test 1
-const scoreDolphins = calcAverage(44, 23, 71);
-const scoreKoalas = calcAverage(65, 54, 49);
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
 console.log(scoreDolphins, scoreKoalas);
 
 const checkWinner = function (avgDolphins, avgKoalas) {
   if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
   } else if (avgKoalas >= 2 * avgDolphins) {
-    console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`);
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
   } else {
     console.log('No team wins...');
   }
 };
 checkWinner(scoreDolphins, scoreKoalas);
+
 checkWinner(576, 111);
 
 // Test 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
+*/
